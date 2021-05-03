b47edb8ca4e52983603be7e73387501fe1c93308
ColinHeyduk <colinheyduk@gmail.com>
2021-05-03 09:20:11 -0400

    uh

---
+++ MusicPlayer/AboutUserDieting.swift
@@ -0,0 +1,22 @@
+//
+//  AboutUserDieting.swift
+//  MusicPlayer
+//
+//  Created by  on 4/28/21.
+//  Copyright © 2021 MoApps. All rights reserved.
+//
+
+import UIKit
+
+class AboutUserDieting: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
---
+++ MusicPlayer/AboutUserWorkout.swift
@@ -0,0 +1,25 @@
+//
+//  AboutUserWorkout.swift
+//  MusicPlayer
+//
+//  Created by  on 4/28/21.
+//  Copyright © 2021 MoApps. All rights reserved.
+//
+
+import UIKit
+
+class AboutUserWorkout: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func HeightSlider(_ sender: UISlider) {
+        
+    }
+    
+}
