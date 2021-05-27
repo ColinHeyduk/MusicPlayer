6b85f57d3e1460a87973392707a61890b05bfcc4
ColinHeyduk <colinheyduk@gmail.com>
2021-05-27 14:19:17 -0400

    fixed gifs

--- MusicPlayer/Workouts.swift
+++ MusicPlayer/Workouts.swift
@@ -15,8 +15,22 @@ class Workouts: UIViewController
     {
         
         super.viewDidLoad()
-
-       
+        
+       let slimGif = UIImage.gifImageWithName("simpsonsskinnygif")
+       let imageView2 = UIImageView(image: slimGif)
+       imageView2.frame = CGRect(x: 90.0, y: 200.0, width: self.view.frame.size.width - 177, height: 160.0)
+       view.addSubview(imageView2)
+        
+        let jackedGif = UIImage.gifImageWithName("duffmangif")
+        let imageView3 = UIImageView(image: jackedGif)
+        imageView3.frame = CGRect(x: 90.0, y: 600.0, width: self.view.frame.size.width - 177, height: 160.0)
+        view.addSubview(imageView3)
+        
+        let athleticGif = UIImage.gifImageWithName("weightbenchcoachgif")
+        let imageView4 = UIImageView(image: athleticGif)
+        imageView4.frame = CGRect(x: 90.0, y: 397.0, width: self.view.frame.size.width - 177, height: 160.0)
+        view.addSubview(imageView4)
+        
     }
     
 
